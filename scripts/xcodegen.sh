80b5f00a48751ae277082470258d789f14bd2dd1
Takuma Osada <judgment9ttk069@gmail.com>
2020-06-07 04:10:35 +0900

    use mint to run xcodegen

--- scripts/xcodegen.sh
+++ scripts/xcodegen.sh
@@ -3,6 +3,6 @@
 set -eu
 
 echo "Generate xcodeproj file by XcodeGen...."
-xcodegen generate --use-cache
+mint run xcodegen generate --use-cache
 carthage bootstrap --platform iOS --no-use-binaries --cache-builds
 bundle exec pod install
