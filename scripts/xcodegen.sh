84a3e0f6a1876708adb9d18f25baee1fdaa7f20f
Takuma Osada <judgment9ttk069@gmail.com>
2020-06-07 03:53:32 +0900

    fix echo sentence

--- scripts/xcodegen.sh
+++ scripts/xcodegen.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-echo "  + Generate xcodeproject by XcodeGen."
+echo "Generate xcodeproj file by XcodeGen...."
 xcodegen generate --use-cache
 carthage bootstrap --platform iOS --no-use-binaries --cache-builds
 bundle exec pod install
