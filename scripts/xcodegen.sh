7b148a523f09e811cc028ecb69b67809d75febc5
Takuma Osada <judgment9ttk069@gmail.com>
2020-06-09 22:33:24 +0900

    fix filter of carthage & cocoapods

--- scripts/xcodegen.sh
+++ scripts/xcodegen.sh
@@ -6,13 +6,13 @@ echo "Generate xcodeproj file by XcodeGen...."
 
 mint run xcodegen generate --use-cache
 
-if [ $(which carthage) ]; then
+if [ -f "Cartfile" ]; then
   carthage bootstrap --platform iOS --no-use-binaries --cache-builds
 else
   "carthage not found"
 fi
 
-if [ $(which pod) ]; then
+if [ -f "Podfile" ]; then
   bundle exec pod install
 else
   "cocoapods not found"
